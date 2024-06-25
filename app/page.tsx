5976867117f57d50b9356deda3ae2811838bbeb9
Masamist <msm-k0304@hotmail.com>
2024-06-25 22:16:02 +1200

    working on route protectuin

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import MainTitle from '@/components/MainTitle'
+import { getTokens } from "next-firebase-auth-edge";
+import { cookies } from "next/headers";
+import { notFound } from "next/navigation";
+import { clientConfig, serverConfig } from "../config";
 
-const Dashboard = () => {
+
+export default async function Dashboard(){
+  const tokens = await getTokens(cookies(), {
+    apiKey: clientConfig.apiKey,
+    cookieName: serverConfig.cookieName,
+    cookieSignatureKeys: serverConfig.cookieSignatureKeys,
+    serviceAccount: serverConfig.serviceAccount,
+  });
+
+  if (!tokens) {
+    notFound();
+  }
   return (
     <main className='container max-w-screen-lg'>
       <MainTitle title='Dashboard' />
     </main>
   )
 }
-
-export default Dashboard
